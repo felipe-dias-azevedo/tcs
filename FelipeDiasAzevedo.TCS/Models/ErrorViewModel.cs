99032dd3c09008d821af16c5b2d1a9b08e62bca3
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2025-05-16 19:24:04 -0300

    start project

---
+++ FelipeDiasAzevedo.TCS/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FelipeDiasAzevedo.TCS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
