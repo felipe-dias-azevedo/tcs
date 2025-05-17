60a0dcca651aef71839acbc3ad2b47b7a1c28486
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2025-05-17 10:47:40 -0300

    feat: add doc for interface

--- FelipeDiasAzevedo.TCS.Services/Services/IOperationalSystemService.cs
+++ FelipeDiasAzevedo.TCS.Services/Services/IOperationalSystemService.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FelipeDiasAzevedo.TCS.Business.Services;
+﻿namespace FelipeDiasAzevedo.TCS.Business.Services;
 
 public interface IOperationalSystemService
 {
+    /// <summary>
+    /// Check if OS service is currently running
+    /// </summary>
+    /// <param name="serviceName"></param>
+    /// <returns></returns>
     bool IsServiceRunning(string serviceName);
 
+    /// <summary>
+    /// Shutdown Operational System
+    /// </summary>
     void Shutdown();
 }
